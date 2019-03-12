9203f4d931701a92af1027d87d0dc269e9ace9d0
Jackie Lin <jlin00@stuy.edu>
2019-03-11 23:56:13 -0400

    founded errors in partition method and fixed them

--- Quick.java
+++ Quick.java
@@ -2,19 +2,22 @@ import java.util.*;
 
 public class Quick{
   public static int partition(int[] data, int start, int end){
-    if (data.length <= 1) return data.length;
+    if (start == end) return start; //if nothing needs to be sorted, just return index
 
-    int random = (int)(Math.random() * data.length); //selects a random index of data
-    //System.out.println(random);
+    int random = (int)(Math.random() * data.length); //selects a random index of data, index of partition value
+    //System.out.println(random); //for debugging purposes
 
-    int temp = data[0]; //temporary storage to perform swap
-    data[0] = data[random]; //performs the swap
-    data[random] = temp;
+    int pivot = data[random]; //stores the pivot value
+    data[random] = data[start]; //performs the swap with starting index
+    data[start] = pivot;
 
-    //sort array according to partition
+    random = start; //index number of pivot changes to start
+    start++; //incease start by one
+
+    //sort array according to partition value, beginning at start and ending at end
     while (start != end){ //if start is not equivalent to end
-      if (data[start] > data[0]){ //if greater than partition value, move to end
-        temp = data[start]; //temporary storage for data[start]
+      if (data[start] > pivot){ //if greater than partition value, move to end
+        int temp = data[start]; //temporary storage for data[start]
         data[start] = data[end]; //swap with data[end]
         data[end] = temp;
         end--; //decrease end
@@ -24,20 +27,19 @@ public class Quick{
       }
     }
 
-    //move the partition value
-    temp = data[start]; //middle value
-    if (data[0] <= data[start]){
-      int temp_value = data[start - 1]; //swap with value next to middle value
-      data[start - 1] = data[0];
-      data[0] = temp_value;
-      return start - 1;
+    //move the partition value when start == end
+    if (data[start] < pivot){ //if pivot is greater than data[start]
+      data[random] = data[start]; //perform the swap
+      data[start] = pivot;
+      random = start; //index of partition value changes
     }
     else{
-      data[start] = data[0]; //swap with middle value
-      data[0] = temp;
+      data[random] = data[start -1]; //performs the swap
+      data[start - 1] = pivot;
+      random = start - 1; //index of partition value changes
     }
 
-    return start;
+    return random;
   }
 
   /*return the value that is the kth smallest value of the array.*/
@@ -57,7 +59,7 @@ public class Quick{
     System.out.println(quickselect(data1, 1)); //should print 999;
 
     int[]ary = { 2, 10, 15, 23, 0,  5};  //sorted :  {0,2,5,10,15,23}
-   System.out.println(quickselect( ary , 0 ));// would return 0
+   //System.out.println(quickselect( ary , 0 ));// would return 0
    System.out.println(quickselect( ary , 1 ));//  would return 2
    System.out.println(quickselect( ary , 2 ));//  would return 5
    System.out.println(quickselect( ary , 3 ));//  would return 10
